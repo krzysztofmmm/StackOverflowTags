1a24859962e97ff2933a46365e87eec390733fa5
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-04-04 18:49:41 +0200

    Initialize SQLite database and EF Core migrations
    Set up the SQLite database using Entity Framework Core, including the initial migration to define the schema based on the Tag model, laying the groundwork for data persistence.

---
+++ StackOverflowTags/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StackOverflowTags.Models;
+
+namespace StackOverflowTags.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ StackOverflowTags/Migrations/20240403113535_createdatabase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StackOverflowTags.Migrations
+{
+    /// <inheritdoc />
+    public partial class createdatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Count = table.Column<int>(type: "INTEGER", nullable: false),
+                    Percentage = table.Column<double>(type: "REAL", nullable: false),
+                    HasSynonyms = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsModeratorOnly = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsRequired = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
