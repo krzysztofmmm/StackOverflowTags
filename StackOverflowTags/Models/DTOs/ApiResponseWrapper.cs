48be7392b035cc5a1dcfb6386710e7033b1a1510
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-04-04 18:48:52 +0200

    Add ApiResponseWrapper DTO for StackOverflow API responses
    Added a Data Transfer Object (DTO) for handling the deserialization of responses from the StackOverflow API, specifically tailored to capture the list of tags and pagination details.

---
+++ StackOverflowTags/Models/DTOs/ApiResponseWrapper.cs
@@ -0,0 +1,12 @@
+﻿using StackOverflowTags.Models.DTOs;
+
+namespace StackOverflowTags.DTOs
+{
+    public class ApiResponseWrapper
+    {
+        public List<ApiTag> Items { get; set; }
+        public bool HasMore { get; set; }
+        public int QuotaMax { get; set; }
+        public int QuotaRemaining { get; set; }
+    }
+}
