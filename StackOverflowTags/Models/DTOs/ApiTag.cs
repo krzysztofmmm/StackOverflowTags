fae7c7a46baddeefb86afbe28c3ca6ad45018d6e
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-04-04 18:49:10 +0200

    Add ApiTag DTO for individual tag details in API responses
    Implemented the ApiTag DTO to accurately represent the structure of tag data within the responses from the StackOverflow API, facilitating easier data handling and transformation.

---
+++ StackOverflowTags/Models/DTOs/ApiTag.cs
@@ -0,0 +1,11 @@
+﻿namespace StackOverflowTags.Models.DTOs
+{
+    public class ApiTag
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public bool HasSynonyms { get; set; }
+        public bool IsModeratorOnly { get; set; }
+        public bool IsRequired { get; set; }
+    }
+}
