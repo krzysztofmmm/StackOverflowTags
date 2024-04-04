e77e70d43863e5eee47aa5c0592db36e2de4c650
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-04-04 18:48:32 +0200

    Add Tag model for representing StackOverflow tags
     Introduced the Tag entity model to encapsulate the properties of a tag fetched from the StackOverflow API, including its name, count, and related statistics.

---
+++ StackOverflowTags/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace StackOverflowTags.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public double Percentage { get; set; }
+        public bool HasSynonyms { get; set; }
+        public bool IsModeratorOnly { get; set; }
+        public bool IsRequired { get; set; }
+    }
+}
